93f089ba39ffb0c1e2de4ca35198afd06c7cb84c
gsista-booth <gsista@chicagobooth.edu>
2022-10-15 07:05:09 +0000

    date formatted

--- date_formatted.rb
+++ date_formatted.rb
@@ -1,3 +1,9 @@
 # Should format and identify different parts of todays date.
-# 
+#
 # "The year is: 2022, the calendar day is: 1, and the month is: 7."
+
+require "date"
+
+todays_date = Date.today
+
+p "The year is: " + todays_date.year.to_s + ", the calendar day is: " + todays_date.month.to_s + ", and the month is: " + todays_date.day.to_s + "."
